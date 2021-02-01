956d41f2060e0fcc2eab1aa4732f091889a54ab5
unknown <singh.avvi98@gmail.com>
2021-02-02 00:37:41 +1100

    Add and delete profile education

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -242,4 +242,88 @@ router.delete("/experience/:exp_id", auth, async (req, res) => {
   }
 });
 
+// @route  PUT api/profile/education
+//@desc    Add profile education
+//@access  Private
+router.put(
+  "/education",
+  [
+    auth,
+    [
+      check("school", "A School Name is necessary").not().isEmpty(),
+      check("degree", "A Degree Title is necessary").not().isEmpty(),
+      check("fieldofstudy", "A Field of Study is necessary").not().isEmpty(),
+      check("from", "A start date is necessary").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description,
+    } = req.body;
+
+    const newEducation = {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description,
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      profile.education.unshift(newEducation);
+
+      await profile.save();
+
+      res.json(profile);
+    } catch (e) {
+      console.error(e.message);
+      res
+        .status(500)
+        .send(
+          "An error has occurred on our end. We apologise for the inconvcenience"
+        );
+    }
+  }
+);
+
+// @route  DELETE api/profile/education/:edu_id
+//@desc    Delete education from profile
+//@access  Private
+router.delete("/education/:edu_id", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+    const removeIndex = profile.education
+      .map((item) => item.id)
+      .indexOf(req.params.edu_id);
+
+    profile.education.splice(removeIndex, 1);
+
+    await profile.save();
+
+    res.json(profile);
+  } catch (e) {
+    console.error(e.message);
+    res
+      .status(500)
+      .send(
+        "An error has occurred on our end. We apologise for the inconvcenience"
+      );
+  }
+});
+
 module.exports = router;
