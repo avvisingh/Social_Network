cbf5882d97ae6719b8976926ece17c737507751f
unknown <singh.avvi98@gmail.com>
2021-02-02 01:34:18 +1100

    User an remove post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -85,4 +85,37 @@ router.get("/:id", auth, async (req, res) => {
   }
 });
 
+// @route  DELETE api/posts/:_id
+//@desc    Delete a post
+//@access  Private
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: "Post could not be found" });
+    }
+
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "Not Authorised!" });
+    }
+
+    await post.remove();
+
+    res.json({ msg: "Post removed" });
+  } catch (e) {
+    console.error(e.message);
+
+    if (e.kind == "ObjectId") {
+      return res.status(404).json({ msg: "Post could not be found" });
+    }
+
+    res
+      .status(500)
+      .send(
+        "Something went wrong with our server. We apologise for the inconvenience."
+      );
+  }
+});
+
 module.exports = router;
