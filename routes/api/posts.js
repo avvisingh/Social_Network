c9bb8d3b5f4a9d73f4aac63be73aedc8ebc114cb
unknown <singh.avvi98@gmail.com>
2021-02-02 01:16:25 +1100

    User can now create posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,11 +1,46 @@
-const express = require('express');
+const auth = require("../../middleware/auth");
+const Post = require("../../models/Post");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+const express = require("express");
+const { check, validationResult } = require("express-validator");
+
 const router = express.Router();
 
-// @route  GET api/users
-//@desc    Test route
-//@access  Public
-router.get('/', (req, res) => {
-    res.send('Posts Route');
-})
+// @route  POST api/posts
+//@desc    Create a post
+//@access  Private
+router.post(
+  "/",
+  [auth, [check("text", "You must input text.").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+
+      const post = await newPost.save();
+
+      res.json(post);
+    } catch (e) {
+      console.error(e.message);
+      res
+        .status(500)
+        .send(
+          "Something went wrong with our server. We apologise for the inconvenience."
+        );
+    }
+  }
+);
 
 module.exports = router;
