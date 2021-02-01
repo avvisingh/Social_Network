679fc1d25d6bae27765f332e455dda3511131ac8
unknown <singh.avvi98@gmail.com>
2021-02-02 01:24:14 +1100

    User can fetch all posts or single post by id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -43,4 +43,46 @@ router.post(
   }
 );
 
+// @route  GET api/posts
+//@desc    Get all posts
+//@access  Private
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (e) {
+    console.error(e.message);
+    res
+      .status(500)
+      .send(
+        "Something went wrong with our server. We apologise for the inconvenience."
+      );
+  }
+});
+
+// @route  GET api/posts/:_id
+//@desc    Get post by id
+//@access  Private
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: "Post could not be found" });
+    }
+
+    res.json(post);
+  } catch (e) {
+    console.error(e.message);
+    if (err.kind == "ObjectId") {
+      return res.status(404).json({ msg: "Post could not be found" });
+    }
+    res
+      .status(500)
+      .send(
+        "Something went wrong with our server. We apologise for the inconvenience."
+      );
+  }
+});
+
 module.exports = router;
